ba98c53add5ac989debf88fa2d5517d73b45eb3e
fuadop <fuadolatunji@gmail.com>
2020-10-14 13:24:22 +0100

    completed practice route

--- client/src/components/Practice/Practice.tsx
+++ client/src/components/Practice/Practice.tsx
@@ -1,5 +1,7 @@
 import React, { FC, useEffect, useRef, useState } from "react";
 import randomWords from "random-words";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import Navbar from "../Navbar";
 import { ImageStyle } from "../interfaces";
 import refreshImage from "../images/refresh-page-option.svg";
@@ -31,10 +33,50 @@ function fetchWords(): any {
 }
 
 
+
 const Practice: FC = () => {
   const textInput = useRef<HTMLInputElement>(null);
   const words = useRef<HTMLParagraphElement>(null);
-  const [count, setCount] = useState(0);
+  // const modal = useRef<HTMLDivElement>(null);
+  const [count, setCount] = useState<number>(0);
+  const [wpmCount, setWpmCount] = useState<string>("0");
+  const [errorCount, setErrorCount] = useState<number>(0);
+  const [timeStamp, setTimeStamp] = useState<number>(0);
+
+  useEffect(() => {
+    if (count != 0) {
+      let isError: boolean = Array.from(
+        words.current!.children[count - 1].children
+      ).some(
+        (child) =>
+          child.classList.contains("bg-danger") &&
+          !child.classList.contains("whitespace")
+      );
+
+      if (isError) {
+        words.current!.children[count - 1].classList.add("contains-error");
+      }
+    }
+  }, [count]);
+
+  const countErrors = () => {
+    setErrorCount(0);
+    Array.from(words.current!.children).forEach((child) => {
+      if (
+        child.classList.contains("contains-error") &&
+        !child.classList.contains("whitespace")
+      ) {
+        setErrorCount((prev) => (prev += 1));
+      }
+    });
+  };
+
+  const calculateStats = () => {
+    countErrors();
+    let wpm =
+      (count - errorCount) / ((new Date().getTime() - timeStamp) / 60000);
+    setWpmCount(wpm.toPrecision(4));
+  };
 
   const onchangeevent = () => {
     let text = textInput.current!.value;
@@ -55,16 +97,12 @@ const Practice: FC = () => {
           );
         }
 
-        if (
-          words.current!.children[count].textContent!.length === text.length
-        ) {
-          document.addEventListener("keyup", (e) => {
-            if (e.code === "Space") {
-              setCount(count + 1);
-              textInput.current!.value = "";
-            }
-          });
-        }
+        document.addEventListener("keyup", (e) => {
+          if (e.code === "Space") {
+            setCount(count + 1);
+            textInput.current!.value = "";
+          }
+        });
       }
     });
   };
@@ -90,11 +128,22 @@ const Practice: FC = () => {
               className="form-control"
               ref={textInput}
               onChange={onchangeevent}
+              onFocus={() => {
+                setTimeStamp(new Date().getTime());
+              }}
             />
           </div>
           <div className="col-4 row">
             <div className="text-center mx-3">
-              <button className="btn btn-primary">1:59</button>
+              <button
+                className="btn btn-primary"
+                data-target="#stats"
+                data-toggle="modal"
+                onClick={calculateStats}
+              >
+                {" "}
+                Check Stats{" "}
+              </button>
             </div>
             <div className="text-center mx-3">
               <a className="btn btn-primary align-middle px-2" href="/practice">
@@ -104,6 +153,37 @@ const Practice: FC = () => {
                   style={refreshImageStyle}
                 />
               </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="modal"
+        id="stats"
+        role="dialog"
+        onClick={(e: any) => {
+          if (e.target.classList.contains("modal")) {
+            window.location.reload();
+          }
+        }}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-body">
+              <div className="row">
+                <div className="col-4 text-center text-uppercase">
+                  <span className="font-weight-bold">{wpmCount} </span>
+                  wpm
+                </div>
+                <div className="col-4 text-center text-uppercase">
+                  <span className="font-weight-bold">{errorCount} </span>
+                  error words
+                </div>
+                <div className="col-4 text-center text-uppercase">
+                  <span className="font-weight-bold">{count} </span>
+                  words typed
+                </div>
+              </div>
             </div>
           </div>
         </div>
