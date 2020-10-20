ea6f3943781a0ed6262aaa7ea34cd3e84115f450
fuadop <fuadolatunji@gmail.com>
2020-10-20 01:50:08 +0100

    fix: added uniqueness to name of rooms

--- server/src/resolvers/Mutation.ts
+++ server/src/resolvers/Mutation.ts
@@ -14,26 +14,27 @@ import randomWords from "random-words";
 import * as bcrypt from "bcrypt";
 
 const Mutation = {
-    createRoom: (_parent: any, {data}: {data: ICreateRoomInput}, {Room, pb}: IContext, _info: any) => {
+    createRoom: (_parent: any, {data}: {data: ICreateRoomInput}, {Room}: IContext, _info: any) => {
         if(data.password.trim() === "" || data.password === null || data.password === undefined) {
             throw new Error("Password can't be empty");
         }
-        const newRoom = new Room({
-            player1: data.player1,
-            name: data.name,
-            words: randomWords(200) as string[],
-            password: bcrypt.hashSync(`${data.password}`, 10),
-            isStarted: false
+        return Room.findOne({name: data.name}).then(exists => {
+            if(exists) {
+                throw new Error("Name has already been used");
+            }
+            const newRoom = new Room({
+                player1: data.player1,
+                name: data.name,
+                words: randomWords(200) as string[],
+                password: bcrypt.hashSync(`${data.password}`, 10),
+                isStarted: false
+            })
+            return newRoom.save().then((res: IRoom) => {
+                return res;
+            }).catch((err: Error) => {
+                throw new Error(`${err}`);
+            });
         })
-        return newRoom.save().then((res: IRoom) => {
-            pb.publish(`room ${res._id}`, {roomStateChange: {
-                mutation: "CREATED",
-                data: res
-            }})
-            return res;
-        }).catch((err: Error) => {
-            throw new Error(`${err}`);
-        });
     },
     joinRoom: (_parent: any, {data}: {data: IJoinRoomInput}, {Room, pb}: IContext, _info: any) => {
         return Room.findById(data.roomid).then((val: IRoom | null) => {
